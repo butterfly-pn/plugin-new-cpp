2c8c5ca08fba2e59b0419c4a48b79ff74c8edbe7
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-02-13 17:19:05 +0100

    Finally fix install

--- hooks/install.fish
+++ hooks/install.fish
@@ -3,9 +3,9 @@ if not test -d ~/.new
 end
 
 if not test -e ~/.new/Makefile
-  cp ../Makefile.new ~/.new/Makefile
+  cp Makefile.new ~/.new/Makefile
 end
 
 if not test -e ~/.new/main.cpp
-  cp ../main.cpp.new ~/.new/main.cpp
+  cp main.cpp.new ~/.new/main.cpp
 end
