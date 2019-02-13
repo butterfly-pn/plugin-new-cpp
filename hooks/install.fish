754bab6eb8d305dce0cda98f3e061c02cdd3e51a
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-02-13 17:14:37 +0100

    Update install.fish

--- hooks/install.fish
+++ hooks/install.fish
@@ -1,7 +1,11 @@
-if (cd ~/.new) do
-  echo
-else
+if not test -d ~/.new
   mkdir ~/.new
+done  
+
+if not test -e ~/.new/Makefile
   cp Makefile.new ~/.new/Makefile
+done
+
+if not test -e ~/.new/main.cpp
   cp main.cpp.new ~/.new/main.cpp
 done
